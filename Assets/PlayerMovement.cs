9917501c17f0e5580b11463da3637859d436f5ef
Alex Bacon <abacon2@academic.rrc.ca>
2023-09-21 13:03:37 -0500

    built a little maze out of static sprites and added a character with a basic movement script

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector2 input; //Vector to hold input
+    public float speed = 5f; //the force amount on movement
+    public Vector2 normalizedInput; //variable to hold normalized input
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); //get the input from the keyboard
+        normalizedInput = input.normalized; //normalize the input
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter();
+    }
+
+
+    void moveCharacter()
+    {
+        transform.Translate(normalizedInput * speed * Time.deltaTime);
+    }
+
+
+
+}
