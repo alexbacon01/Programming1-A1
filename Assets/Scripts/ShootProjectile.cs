c124785c153fbf73d81de184692fa7a346ed49f2
Alex Bacon <abacon2@academic.rrc.ca>
2023-09-22 15:27:27 -0500

    fixed the projectile collision making the player character change colour

--- Assets/Scripts/ShootProjectile.cs
+++ Assets/Scripts/ShootProjectile.cs
@@ -5,19 +5,16 @@ public class ShootProjectile : MonoBehaviour
     public Rigidbody2D rb; //the rigid body of projectile
     public float forceAmount = 1f; //force amount
     public GameObject target; //game object for target
-    public SpriteRenderer sr; //sprite renderer 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //get componenet rigid body
-        sr = target.GetComponent<SpriteRenderer>(); //get component sprite renderer 
     }
 
     // Update is called once per frame
     void Update()
     {
-        sr.color = Color.blue; //set the target sprite to color blue
     }
     private void FixedUpdate()
     {
@@ -35,7 +32,7 @@ public class ShootProjectile : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        sr.color = Color.red; //when it collides with object change colour to red NOT WORKING
+        collision.gameObject.GetComponent<SpriteRenderer>().color = Color.red; //when it collides with object change colour to red 
         Debug.Log(collision.gameObject.name); //what did it hit??
     }
 }
